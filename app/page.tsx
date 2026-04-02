4641ca2586e01eb6372e84f75b2329f4ba61e85b
dibbojit-das <dibbojit.joy@graypeakstudios.com>
2026-04-02 21:50:39 +0600

    updated the UI components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "@/components/Home/Hero";
 import { Services } from "@/components/Home/Services";
+import { WhyChooseUs } from "@/components/Home/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="flex flex-1 flex-col">
       <Hero />
       <Services />
+      <WhyChooseUs />
     </main>
   );
 }

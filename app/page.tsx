360ec23afe5c7b107bae4ece6f8872203935006f
dibbojit-das <dibbojit.joy@graypeakstudios.com>
2026-04-02 22:10:15 +0600

    Added CTA.tsx file

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Hero } from "@/components/Home/Hero";
 import { Services } from "@/components/Home/Services";
 import { WhyChooseUs } from "@/components/Home/WhyChooseUs";
+import { CTA } from "@/components/Home/CTA";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Services />
       <WhyChooseUs />
+      <CTA />
     </main>
   );
 }
